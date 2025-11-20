394cabdcfa0c5a025227d7a6469608333e89a792
Darshan Prakash <52451615+Darsh70@users.noreply.github.com>
2025-11-19 20:57:54 -0600

    Add more timer labels

--- count_down.gd
+++ count_down.gd
@@ -13,15 +13,14 @@ func _process(_delta: float) -> void:
 
 
 func _on_pause_pressed() -> void:
-	#$Timer.set_paused(true)
 	if not $Timer1.text:
 		$Timer1.text = str("%.2f" % current_time)
 	elif not $Timer2.text:
 		$Timer2.text = str("%.2f" % current_time)
+	elif not $Timer3.text:
+		$Timer3.text = str("%.2f" % current_time)
+	elif not $Timer4.text:
+		$Timer4.text = str("%.2f" % current_time)
+		$Pause.disabled = true
 	$MissileManager.launch_next()
 	$Timer.start()
-
-func _on_start_pressed() -> void:
-	if $Timer.is_paused():
-		$Timer.set_paused(false)
-		
